c637191a383956c6f8ef716851c164942b974e81
Kamil Śliwak <cameel2@gmail.com>
2009-10-28 23:33:18 +0100

    Minor code formatting changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
-int main(int argc, char *argv[])
+
+
+
+int main(int argc, char * argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication application(argc, argv);
+    MainWindow mainWindow;
+    mainWindow.show();
+    return application.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,33 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+
 #include <QtGui/QMainWindow>
+
+
+
 
 namespace Ui
 {
     class MainWindow;
 }
 
+
+
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = 0);
+    MainWindow(QWidget * parent = NULL);
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow * ui;
 };
+
+
+
 
 #endif // MAINWINDOW_H
